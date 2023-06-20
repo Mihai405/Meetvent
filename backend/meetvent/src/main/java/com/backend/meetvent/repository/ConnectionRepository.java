046b6f6ff1137d2333d6d0935b85436fc2b3b83e
Mihai405 <mihaijurj2000@yahoo.com>
2023-06-20 21:36:56 +0300

    Change connection repository methods

--- backend/meetvent/src/main/java/com/backend/meetvent/repository/ConnectionRepository.java
+++ backend/meetvent/src/main/java/com/backend/meetvent/repository/ConnectionRepository.java
@@ -10,9 +10,25 @@ import java.util.Optional;
 
 public interface ConnectionRepository extends JpaRepository<Connection, Long> {
     List<Connection> findAllByAppUser2_IdAndUser1ResponseAndUser2Response(Long appUser2Id, String user1Response, String user2Response);
+    @Query("select c.appUser1.id from Connection c where " +
+            "c.appUser2.id = :appUser2Id and c.user1Response = :user1Response and c.user2Response IS null ")
+    List<Long> findAppUser1IdsByAppUser2_IdAndUser1ResponseAndUser2ResponseIsNotNull(Long appUser2Id, String user1Response);
     List<Connection> findAllByAppUser2_IdAndUser1Response(Long appUser2Id, String user1Response);
     List<Connection> findAllByAppUser2_IdAndUser2Response(Long appUser2Id, String user2Response);
     List<Connection> findAllByAppUser1_Id(Long appUser1Id);
+
+    @Query("select c.appUser2.id from Connection c where " +
+            "c.appUser1.id = :appUser1Id")
+    List<Long> findAppUser2IdsByAppUser1Id(Long appUser1Id);
+
+    @Query("select c.appUser1.id from Connection c where " +
+            "c.appUser2.id = :appUser2Id and c.user2Response IS NOT null")
+    List<Long> findAppUser1IdsByAppUser2IdAndUser2ResponseIsNotNull(Long appUser2Id);
+
+    @Query("select c.appUser1.id from Connection c where " +
+            "c.appUser2.id = :appUser2Id and c.user1Response = :user1Response ")
+    List<Long> findAppUser1IdsByAppUser2IdAndUser1Response(Long appUser2Id, String user1Response);
+
     Optional<Connection> findByAppUser1_IdAndAndAppUser2_Id(Long appUser1Id, Long appUser2Id);
     List<Connection> findAllByAppUser1AndUser1ResponseAndUser2Response(AppUser appUser1Id, String user1Response, String user2Response);
     List<Connection> findAllByAppUser2AndUser1ResponseAndUser2Response(AppUser appUser2Id, String user1Response, String user2Response);
--- backend/meetvent/src/main/java/com/backend/meetvent/service/connection/ConnectionServiceImpl.java
+++ backend/meetvent/src/main/java/com/backend/meetvent/service/connection/ConnectionServiceImpl.java
@@ -12,9 +12,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @Transactional
@@ -32,15 +34,15 @@ public class ConnectionServiceImpl implements ConnectionService {
     }
 
     @Override
+    @Transactional
     public List<AppUser> findUsers(String userToken) {
         List<AppUser> yourNextUsers = new ArrayList<>();
         AppUser appUser = this.appUserService.getUserFromToken(userToken);
-        List<Connection> likeMatches = this.connectionRepository.findAllByAppUser2_IdAndUser1ResponseAndUser2Response(appUser.getId(), "YES", null);
-        List<Long> peopleIds = this.getPeopleWhoLikesYouIdsFromMatches(likeMatches);
-        yourNextUsers.addAll(this.getPeopleWhoLikesYou(peopleIds));
-        List<Long> notWantedProfilesIds = new ArrayList<>();
-        notWantedProfilesIds.addAll(peopleIds);
-        notWantedProfilesIds.addAll(this.alreadyViewedProfilesIds(appUser.getId()));
+        List<Long> peopleWhoLikesYouIds = this.connectionRepository.findAppUser1IdsByAppUser2_IdAndUser1ResponseAndUser2ResponseIsNotNull(appUser.getId(), "YES");
+        yourNextUsers.addAll(this.getPeopleWhoLikesYou(peopleWhoLikesYouIds));
+        List<Long> notWantedProfilesIds = Stream.of(peopleWhoLikesYouIds, this.alreadyViewedProfilesIds(appUser.getId()))
+                                                .flatMap(Collection::stream)
+                                                .collect(Collectors.toList());
         yourNextUsers.addAll(this.getOtherPeople(notWantedProfilesIds, appUser.getId()));
         return yourNextUsers;
     }
@@ -72,14 +74,6 @@ public class ConnectionServiceImpl implements ConnectionService {
         }
     }
 
-    private List<Long> getPeopleWhoLikesYouIdsFromMatches(List<Connection> matches) {
-        ArrayList<Long> peopleIds = new ArrayList<>();
-        for(Connection match:matches) {
-            peopleIds.add(match.getAppUser1().getId());
-        }
-        return peopleIds;
-    }
-
     private List<AppUser> getPeopleWhoLikesYou(List<Long> peopleIds) {
         return this.appUserService.getAppUsersWithIdsInList(peopleIds);
     }
@@ -91,24 +85,12 @@ public class ConnectionServiceImpl implements ConnectionService {
     }
 
     private List<Long> alreadyViewedProfilesIds(Long myId) {
-        List<Connection> seenProfileMatchesFirst = this.connectionRepository.findAllByAppUser1_Id(myId);
-        List<Connection> seenProfileMatchesSecondYes = this.connectionRepository.findAllByAppUser2_IdAndUser2Response(myId, "YES");
-        List<Connection> seenProfileMatchesSecondNO = this.connectionRepository.findAllByAppUser2_IdAndUser2Response(myId, "NO");
-        List<Connection> declineMyProfile = this.connectionRepository.findAllByAppUser2_IdAndUser1Response(myId, "NO");
-        List<Long> idsForSeenUsers = new ArrayList<>();
-        for(Connection match:seenProfileMatchesFirst) {
-            idsForSeenUsers.add(match.getAppUser2().getId());
-        }
-        for(Connection match:seenProfileMatchesSecondYes) {
-            idsForSeenUsers.add(match.getAppUser1().getId());
-        }
-        for(Connection match:seenProfileMatchesSecondNO) {
-            idsForSeenUsers.add(match.getAppUser1().getId());
-        }
-        for(Connection match:declineMyProfile) {
-            idsForSeenUsers.add(match.getAppUser1().getId());
-        }
-        return idsForSeenUsers;
+        List<Long> seenProfilesAsFirstUser = this.connectionRepository.findAppUser2IdsByAppUser1Id(myId);
+        List<Long> seenProfileAsSecondUser = this.connectionRepository.findAppUser1IdsByAppUser2IdAndUser2ResponseIsNotNull(myId);
+        List<Long> declineMyProfile = this.connectionRepository.findAppUser1IdsByAppUser2IdAndUser1Response(myId, "NO");
+        return Stream.of(seenProfilesAsFirstUser, seenProfileAsSecondUser, declineMyProfile)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
     }
 
     private Connection createNewContact(AppUser appUser1, AppUser appUser2, String tinderResponse) {
